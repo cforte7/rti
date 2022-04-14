f43eda4ababcdfcf9facb07ae1b37b7fe0fb981e
cforte7 <cforte7@gmail.com>
2022-04-13 19:59:09 -0500

    does not work but i need it online

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,45 @@ use std::env;
 
 use chrono::prelude::{NaiveDateTime};
 
-struct EpochArg {
-    epoch: i64,
-    parsed_value: NaiveDateTime
+enum InputArgTypes {
+    EpochInput,
+    StringInput,
 }
 
+struct EpochArg {
+    original_input: String,
+}
+const DATETIME_PARSE_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+
 impl EpochArg {
-    fn new(epoch: i64) -> EpochArg {
-        EpochArg{epoch, parsed_value: NaiveDateTime::from_timestamp(epoch , 0)}
+    fn new(original_input: String) -> EpochArg {
+        EpochArg{original_input}
     }
 
     fn fmt_for_user(&self) -> String {
-        format!("{} => {}", self.epoch, self.parsed_value.format("%Y-%m-%d %H:%M:%S").to_string())
+        format!("{} => {}", self.original_input, self.parsed_value.format("%Y-%m-%d %H:%M:%S")).to_string()
     }
 }
 
 
 
-fn main() {
-    let input: Vec<String> = env::args().collect();
+fn parse_inputs(input_args: Vec<String>) -> Vec<EpochArg> {
+    // Take in arbitrary number of inputs and convert them to EpochArg struct
     let mut parsed_input = vec![];
 
     // first arg is always path to file which we do not want
-    for elem in input[1..input.len()].iter() {
-        let parsed_elem = elem.parse::<i64>().unwrap();
-        parsed_input.push(EpochArg::new(parsed_elem))
+    for elem in input_args[1..input_args.len()].iter() {
+        parsed_input.push(EpochArg::new(elem.to_string()));
     }
+    parsed_input
+}
 
-    for i in parsed_input {
-        println!("{}", i.fmt_for_user())
+fn main() {
+    let input: Vec<String> = env::args().collect();
+    let parsed_inputs = parse_inputs(input);
+
+    for i in parsed_inputs {
+        println!("{}", i.fmt_for_user());
     }
 
 }
