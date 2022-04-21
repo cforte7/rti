c04f73bcfab8fed14450c0c9d0bddc437c96af68
cforte7 <cforte7@gmail.com>
2022-04-21 13:55:57 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,9 @@ use chrono::prelude::NaiveDateTime;
 use std::env;
 
 mod datetime_parsing;
-use datetime_parsing::datetime_parsing::parse_datetime;
+use datetime_parsing::datetime_parsing::parse_arg;
 
 const DATETIME_PARSE_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
-
-struct EpochArg {
-    arg: String,
-}
 
 fn epoch_to_datetime(epoch: i64) -> String {
     // take in epoch time and return datetime as string
@@ -18,49 +14,19 @@ fn epoch_to_datetime(epoch: i64) -> String {
     datetime.format(DATETIME_PARSE_FORMAT).to_string()
 }
 
-// fn parse_string(arg: &String) -> String {
-//     // take in datetime string and return epoch as string
-//     match arg.to_lowercase().as_str() {
-//         "now" => Local::now().timestamp().to_string(),
-//         _ => "Unsupported keyword".to_string(),
-//     }
-// }
+fn fmt_and_print(arg: String) {
+    let maybe_int_parse = arg.parse::<i64>();
+    let parsed_value = match maybe_int_parse {
+        Ok(val) => epoch_to_datetime(val),
+        Err(_) => parse_arg(&arg),
+    };
 
-impl EpochArg {
-    fn new(arg: String) -> EpochArg {
-        EpochArg { arg }
-    }
-
-    fn fmt_for_user(&self) -> String {
-        let maybe_int_parse = self.arg.parse::<i64>();
-        let parsed_value = match maybe_int_parse {
-            Ok(val) => epoch_to_datetime(val),
-            Err(_) => parse_datetime(&self.arg),
-        };
-
-        format!("{} => {}", self.arg, parsed_value).to_string()
-    }
-}
-
-fn parse_inputs(input_args: Vec<String>) -> Vec<EpochArg> {
-    // Take in arbitrary number of inputs and convert them to EpochArg
-    let mut parsed_input = vec![];
-
-    // first arg is always path to file which we do not want
-    // TODO: change it so the user separates args with a delimiter
-    // and then combine the args into one string then split back out
-    // to allow entires with spaces such as a datetime e.g. '05-24-1993 05:22AM'
-    for elem in input_args[1..input_args.len()].iter() {
-        parsed_input.push(EpochArg::new(elem.to_string()));
-    }
-    parsed_input
+    println!("{}", format!("{} => {}", arg, parsed_value));
 }
 
 fn main() {
     let input: Vec<String> = env::args().collect();
-    let parsed_inputs: Vec<EpochArg> = parse_inputs(input);
-
-    for i in parsed_inputs {
-        println!("{}", i.fmt_for_user());
+    for elem in input[1..].iter() {
+        fmt_and_print(elem.to_string());
     }
 }
