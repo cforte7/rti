d11cac81ffed7e456e58b4e58e07451d019d8578
Chris <cforte7@gmail.com>
2022-05-04 17:06:33 -0500

    working but gross

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,9 +1,10 @@
 pub mod config {
+    use chrono_tz::{ParseError, Tz};
     use confy;
     use serde::{Deserialize, Serialize};
     #[derive(Debug, Serialize, Deserialize)]
     pub struct MyConfig {
-        default_timezone: Option<String>,
+        pub default_timezone: Option<String>,
     }
 
     impl ::std::default::Default for MyConfig {
@@ -15,5 +16,33 @@ pub mod config {
     }
     pub fn load_config() -> MyConfig {
         confy::load("rti").expect("Unable to load config.")
+    }
+
+    pub fn set_tz_config(tz_input: String) {
+        // Verify that this can be parsed as a timezone. Notify and exit if it can't.
+        let parse_result: Result<Tz, ParseError> = tz_input.parse();
+        if let Err(_) = parse_result {
+            println!("Invalid Timezone! Settings not updated.");
+            return;
+        }
+        let existing_config = load_config();
+        let new_config = MyConfig {
+            default_timezone: Some(tz_input.to_string()),
+            ..existing_config
+        };
+        if let Ok(_) = confy::store("rti", new_config) {
+            println!("Timezone updated to {}", tz_input)
+        }
+    }
+
+    pub fn clear_tz_config() {
+        let existing_config = load_config();
+        let new_config = MyConfig {
+            default_timezone: None,
+            ..existing_config
+        };
+        if let Ok(_) = confy::store("rti", new_config) {
+            println!("Timezone cleared.")
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate chrono;
 use chrono::format::Parsed;
-use chrono::prelude::Local;
-use chrono_tz::TZ_VARIANTS;
-
+use chrono::prelude::{DateTime, Local, NaiveDateTime};
+use chrono_tz::Tz;
 mod config;
-use config::config::load_config;
+use config::config::{clear_tz_config, load_config, set_tz_config, MyConfig};
 
 use std::env;
 
@@ -13,24 +12,36 @@ use datetime_parsing::datetime_parsing::parse_arg;
 
 const DATETIME_PARSE_FORMAT: &str = "%m-%d-%Y %H:%M:%S";
 
-fn epoch_to_datetime(epoch: i64) -> String {
+fn epoch_to_datetime(epoch: i64, config: &MyConfig) -> String {
     // take in epoch time and return datetime as string formatted for system timezone.
     // todo: allow env var to specify timezone
     let mut parsed = Parsed::new();
     parsed.set_timestamp(epoch).unwrap();
-    let local_time = Local::now();
-    let timezone_offset = local_time.offset();
-    parsed
-        .to_datetime_with_timezone(timezone_offset)
-        .unwrap()
-        .format(DATETIME_PARSE_FORMAT)
-        .to_string()
+    match &config.default_timezone {
+        Some(tz) => {
+            let tz: Tz = tz.parse().unwrap();
+            let timezone_offset = tz;
+            parsed
+                .to_datetime_with_timezone(&timezone_offset)
+                .unwrap()
+                .format(DATETIME_PARSE_FORMAT)
+                .to_string()
+        }
+        None => {
+            let local_offset = Local::now();
+            parsed
+                .to_datetime_with_timezone(&local_offset.timezone())
+                .unwrap()
+                .format(DATETIME_PARSE_FORMAT)
+                .to_string()
+        } // parsed.to_datetime_with_timezone(&timezone_offset).unwrap().format(DATETIME_PARSE_FORMAT).to_string()
+    }
 }
 
-fn fmt_and_print(arg: String) {
+fn fmt_and_print(arg: String, config: &MyConfig) {
     let maybe_int_parse = arg.parse::<i64>();
     let parsed_value = match maybe_int_parse {
-        Ok(val) => epoch_to_datetime(val),
+        Ok(val) => epoch_to_datetime(val, config),
         Err(_) => parse_arg(&arg),
     };
 
@@ -38,13 +49,20 @@ fn fmt_and_print(arg: String) {
 }
 
 fn main() {
-    for tz in TZ_VARIANTS {
-        println!("{:?}", tz);
+    let input: Vec<String> = env::args().collect();
+
+    if input[1] == "set-tz" {
+        let tz_arg: String = input[2].parse().unwrap();
+        match tz_arg.as_str() {
+            "clear" => clear_tz_config(),
+            _ => set_tz_config(tz_arg.to_string()),
+        };
+        return;
     }
+
     let config = load_config();
     println!("{:?}", config);
-    let input: Vec<String> = env::args().collect();
     for elem in input[1..].iter() {
-        fmt_and_print(elem.to_string());
+        fmt_and_print(elem.to_string(), &config);
     }
 }
