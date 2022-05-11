8a7f21a50a3d7e1c89d30b7e77244b4f6222b109
Chris <cforte7@gmail.com>
2022-05-11 07:45:18 -0500

    add help text

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,19 @@ fn fmt_and_print(arg: String, tz: &Tz, custom_tokens: &Vec<String>) {
     println!("{}", format!("{} => {}", arg, parsed_value));
 }
 
+
+fn help() {
+    println!("RTI converts Unix epoch time to a human readable format and vice versa.");
+    println!("Enter values to convert separated by a space.\n");
+    println!("Additional commands:");
+    println!("    help -  View this message.");
+    println!("    set-tz - Set a configured timezone. Uses first argument after set-tz.");
+    println!("    clear-tz - Clear timezone config.");
+    println!("    add-token - Add a custom parsing token. Uses first argument after add-token. See https://docs.rs/chrono/0.4.0/chrono/format/strftime/index.html for syntax.");
+    println!("    remove-token - Remove a custom parsing token. No changes made if the token doesn't exist.");
+    println!("    view-tokens - See a list of stored custom parsing tokens.");
+}
+
 fn main() {
     let input: Vec<String> = env::args().collect();
 
@@ -36,6 +49,7 @@ fn main() {
     }
     let maybe_keyword: String = input[1].parse().unwrap();
     match maybe_keyword.as_str() {
+        "help" => help(),
         "set-tz" => set_tz_config(input[2].parse().unwrap()),
         "clear-tz" => clear_tz_config(),
         "add-token" => add_custom_token(input[2].parse().unwrap()),
